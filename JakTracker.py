582a147f0ae53d375436841b533d4995158dc03d
dallmeyer <2515356+dallmeyer@users.noreply.github.com>
2022-11-27 13:23:26 -0800

    separate icons for everything

--- JakTracker.py
+++ JakTracker.py
@@ -3,153 +3,7 @@ from PIL import Image
 import io
 import time
 import PySimpleGUI as PSG
-
-# layout for tracker UI
-LAYOUT = [
-  [ # totals & misc
-    'num_power_cells',
-    'num_orbs',
-    'num_scout_flies',
-    # 'blank',
-    # 'res_jungle_eggtop',
-    # 'res_jungle_fishgame',
-    # 'res_snow_eggtop',
-  ],
-  'HSeparator',
-  [ # Geyser Rock
-    'res_training_gimmie',
-    'res_training_door',
-    'res_training_climb',
-    'res_training_buzzer',
-  ],
-  [ # Sandover Village
-    'res_village1_mayor_money',
-    'res_village1_uncle_money',
-    'res_village1_yakow',
-    'res_village1_oracle_money1',
-    'res_village1_oracle_money2',
-    'res_village1_buzzer',
-  ],
-  [ # Sentinel Beach
-    'res_beach_ecorocks',
-    'res_beach_flutflut',
-    'res_beach_pelican',
-    'res_beach_seagull',
-    'res_beach_cannon',
-    'res_beach_gimmie',
-    'res_beach_sentinel',
-    'res_beach_buzzer',
-  ],
-  [ # Forbidden Jungle
-    'res_jungle_lurkerm', # mirrors
-    'res_jungle_tower',
-    'res_jungle_eggtop',  # blue eco switch
-    'res_jungle_plant',
-    'res_jungle_fishgame',
-    'res_jungle_canyon_end',
-    'res_jungle_temple_door',
-    'res_jungle_buzzer',
-  ],
-  [ # Misty Island
-    'res_misty_muse',
-    'res_misty_boat', # top of lurker ship
-    'res_misty_cannon',
-    'res_misty_warehouse', # return to dark eco pool
-    'res_misty_bike', # flying lurkers
-    'res_misty_bike_jump',  # zoomer jump
-    'res_misty_eco_challenge',  # boosted
-    'res_misty_buzzer',
-  ],
-  [ # Fire Canyon
-    'res_firecanyon_end',
-    'res_firecanyon_buzzer',
-  ],
-  [ # Rock Village
-    'res_village2_gambler_money',
-    'res_village2_geologist_money',
-    'res_village2_warrior_money',
-    'res_village2_oracle_money1',
-    'res_village2_oracle_money2',
-    'res_village2_buzzer',
-  ],
-  [ # Lost Precusor City
-    'res_sunken_room',  # raise the chamber
-    'res_sunken_pipe',  # pipegame
-    'res_sunken_slide', # bottom of LPC
-    'res_sunken_sharks',  # quickly cross dangerous pool (next to button skip)
-    'res_sunken_platforms', # puzzle
-    'res_sunken_top_of_helix',  # climb from bottom (piggyback)
-    'res_sunken_spinning_room', # center of the complex (above dark eco)
-    'res_sunken_buzzer',
-  ],
-  [ # Boggy Swamp
-    'res_swamp_flutflut',
-    'res_swamp_billy',
-    'res_swamp_battle',
-    'res_swamp_tether_4', # yep this is the order in start menu >_<
-    'res_swamp_tether_1',
-    'res_swamp_tether_2',
-    'res_swamp_tether_3',
-    'res_swamp_buzzer',
-  ],
-  [ # Precursor Basin
-    'res_rolling_moles',
-    'res_rolling_robbers',
-    'res_rolling_race',
-    'res_rolling_lake',
-    'res_rolling_plants',
-    'res_rolling_ring_chase_1',
-    'res_rolling_ring_chase_2',
-    'res_rolling_buzzer',
-  ],
-  [ # Mountain Pass
-    'res_ogre_boss',  # klaww
-    'res_ogre_end',
-    'res_ogre_secret',  # backwards cell / stalag hops / tree hops / boulder skip
-    'res_ogre_buzzer',
-  ],
-  [ # Volcanic Crater
-    'res_village3_miner_money1',
-    'res_village3_miner_money2',
-    'res_village3_miner_money3',
-    'res_village3_miner_money4',
-    'res_village3_oracle_money1',
-    'res_village3_oracle_money2',
-    'res_village3_extra1',  # secret cell (from spider cave)
-    'res_village3_buzzer',
-  ],
-  [ # Snowy Mountain
-    'res_snow_eggtop',  # yellow eco switch
-    'res_snow_ram', # lurker troops
-    'res_snow_bumpers', # blockers
-    'res_snow_cage',  # frozen crate (yellow eco/flutflut)
-    'res_snow_fort',  # inside fortress
-    'res_snow_ball',  # open fortress gate
-    'res_snow_bunnies',
-    'res_snow_buzzer',
-  ],
-  [ # Spider Cave
-    'res_cave_gnawers',
-    'res_cave_dark_crystals',
-    'res_cave_dark_climb',
-    'res_cave_robot_climb',
-    'res_cave_swing_poles',
-    'res_cave_spider_tunnel',
-    'res_cave_platforms', # quad jump
-    'res_cave_buzzer',
-  ],
-  [ # Lava Tube
-    'res_lavatube_end',
-    'res_lavatube_buzzer',
-  ],
-  [ # Citadel
-    'res_citadel_sage_blue',
-    'res_citadel_sage_red',
-    'res_citadel_sage_yellow',
-    'res_citadel_sage_green',
-    'res_citadel_buzzer',
-  ]
-]
+from layout import LAYOUT
 
 # config for fields to query
 offset_tmp = 0
@@ -242,7 +96,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_training_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -296,7 +150,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_jungle_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -368,7 +222,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_village1_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -422,7 +276,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_beach_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -494,7 +348,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_misty_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -566,7 +420,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_village2_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -638,7 +492,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_swamp_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -698,7 +552,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_sunken_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -770,7 +624,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_rolling_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -842,7 +696,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_snow_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -875,7 +729,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_firecanyon_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -929,7 +783,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_citadel_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -947,7 +801,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_village3_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -1073,7 +927,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_cave_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -1100,7 +954,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_ogre_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
@@ -1127,7 +981,7 @@ FIELDS = {
     'display': '',
     'icon_type': 'cell',
     'icons': [
-      '7flies_cell.png',
+      'res_lavatube_buzzer.png',
     ],
     'offset': (offset_tmp:=offset_tmp+1),
     'length': 1,
